862a202d3dc293ae56d7221cd5b579122c67bb48
Powai Leung <pwleung1489@gmail.com>
2019-09-11 18:16:55 +0000

    input Nomster Lesson 10-39 Add an Image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,7 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:picture)
+    params.require(:photo).permit(:picture, :caption)
   end 
 
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,10 @@ class NotificationMailer < ApplicationMailer
   # set up a default from when email sent
   default from: "no-rely@nomsterapp.com"
   # add template
-  def comment_added
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
     mail(to: "powaileung6@gmail.com", 
-         subject: "A comment has been added to your place")
+         subject: "A comment has been added to #{@place.name}")
   end
 end
