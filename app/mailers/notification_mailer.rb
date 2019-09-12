ac1527f11412ae21a7e99a9c891bc6c7d5bdcea9
Powai Leung <pwleung1489@gmail.com>
2019-09-12 02:09:01 +0000

    input Nomster Lesson 11-40 Emailing Basics

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,10 +2,8 @@ class NotificationMailer < ApplicationMailer
   # set up a default from when email sent
   default from: "no-rely@nomsterapp.com"
   # add template
-  def comment_added(comment)
-    @place = comment.place
-    @place_owner = @place.user
+  def comment_added
     mail(to: "powaileung6@gmail.com", 
-         subject: "A comment has been added to #{@place.name}")
+         subject: "A comment has been added to your place")
   end
 end
