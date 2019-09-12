f8a01366e3c2fe7e5c1ca66a5ff877c7cf6fa264
Powai Leung <pwleung1489@gmail.com>
2019-09-12 20:14:47 +0000

    input Nomster Lesson 11-44 Mailing on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = { host: 'https://nomster-powai-leung.herokuapp.com' }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
